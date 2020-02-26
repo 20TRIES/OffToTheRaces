fb37833c388e81877c2064366213df97850709d8
Marcus Turner <marcus@clicktech.com>
2020-02-26 00:23:10 +0000

    Quick and dirty ui (please don't judge me)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,87 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
 
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
 const app = new Vue({
     el: '#app',
+    data: {
+        tab: 'active',
+        races: {
+            active: [],
+            recent: [],
+            topPerformances: [],
+        }
+    },
+    created: function () {
+        this.load();
+    },
+    methods: {
+        load: function () {
+            this.loadActiveRaces();
+            this.loadRecentRaces();
+            this.loadTopPerformances();
+        },
+        loadActiveRaces: function () {
+            let req;
+            req = new XMLHttpRequest();
+            req.open("GET", "/api/races/active");
+            req.setRequestHeader('Accept', 'application/json');
+            req.setRequestHeader('Content-Type', 'application/json');
+            req.addEventListener('load', (event) => {
+                let data;
+                data = JSON.parse(req.responseText);
+                this.races.active = data.races;
+            });
+            req.send();
+        },
+        loadRecentRaces: function () {
+            let req;
+            req = new XMLHttpRequest();
+            req.open("GET", "/api/races/finished/1500");
+            req.setRequestHeader('Accept', 'application/json');
+            req.setRequestHeader('Content-Type', 'application/json');
+            req.addEventListener('load', (event) => {
+                let data;
+                data = JSON.parse(req.responseText);
+                this.races.recent = data.races;
+            });
+            req.send();
+        },
+        loadTopPerformances: function () {
+            let req;
+            req = new XMLHttpRequest();
+            req.open("GET", "/api/performances/fastest/1500");
+            req.setRequestHeader('Accept', 'application/json');
+            req.setRequestHeader('Content-Type', 'application/json');
+            req.addEventListener('load', (event) => {
+                let data;
+                data = JSON.parse(req.responseText);
+                this.races.topPerformances = data.performances;
+            });
+            req.send();
+        },
+        progressTime: function () {
+            let req;
+            req = new XMLHttpRequest();
+            req.open("PUT", "/api/time");
+            req.setRequestHeader('Accept', 'application/json');
+            req.setRequestHeader('Content-Type', 'application/json');
+            req.addEventListener('load', (event) => {
+                this.load();
+            });
+            req.send();
+        },
+        createRace: function () {
+            let req;
+            req = new XMLHttpRequest();
+            req.open("POST", "/api/race");
+            req.setRequestHeader('Accept', 'application/json');
+            req.setRequestHeader('Content-Type', 'application/json');
+            req.addEventListener('load', (event) => {
+                this.load();
+            });
+            req.send();
+        },
+    }
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">
+                    Dashboard
+                    <button type="button" v-on:click="createRace" style="float: right;">Create Race</button>
+                    <button type="button" v-on:click="progressTime" style="float: right;">Progress</button>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,6 +19,28 @@
                     @endif
 
                     Welcome to the races!
+
+                    <div>
+                        <button v-on:click="tab = 'active'" v-bind:style="tab === 'active' ? 'background: blue;' : ''">Active Races</button>
+                        <button v-on:click="tab = 'recent'" v-bind:style="tab === 'recent' ? 'background: blue;' : ''">Recent Finished Races</button>
+                        <button v-on:click="tab = 'top-performances'" v-bind:style="tab === 'top-performances' ? 'background: blue;' : ''">Top Performances</button>
+                    </div>
+
+                    <div v-if="tab === 'active'">
+                        <pre v-for="race in races.active">
+                            @{{race}}
+                        </pre>
+                    </div>
+                    <div v-if="tab === 'recent'">
+                        <pre v-for="race in races.recent">
+                            @{{race}}
+                        </pre>
+                    </div>
+                    <div v-if="tab === 'top-performances'">
+                        <pre v-for="race in races.topPerformances">
+                            @{{race}}
+                        </pre>
+                    </div>
                 </div>
             </div>
         </div>
